b490b223204c1dcc8d33166e280e444b6b5147c7
sangyun0904 <sangyun0904@gmail.com>
2024-11-28 10:58:32 +0900

    first commit

---
+++ src/main/java/com/sykim/axelrod/AxelrodApplication.java
@@ -0,0 +1,13 @@
+package com.sykim.axelrod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxelrodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxelrodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sykim/axelrod/AxelrodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sykim.axelrod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxelrodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
