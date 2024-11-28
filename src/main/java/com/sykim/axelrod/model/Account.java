1f438f1084b69805cd680b9009e406f70edccd36
sangyun0904 <sangyun0904@gmail.com>
2024-11-28 14:22:14 +0900

    Add postgresql connection, Account model

---
+++ src/main/java/com/sykim/axelrod/model/Account.java
@@ -0,0 +1,18 @@
+package com.sykim.axelrod.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Double balance;
+
+}
---
+++ src/main/java/com/sykim/axelrod/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.sykim.axelrod.repository;
+
+import com.sykim.axelrod.model.Account;
+import org.springframework.data.repository.Repository;
+
+public interface AccountRepository extends Repository<Account, Long> {
+}
