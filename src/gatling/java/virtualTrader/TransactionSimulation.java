c8316537e23ba80424a2d0946d80c1a2b602c5c3
sangyun0904 <sangyun0904@gmail.com>
2025-01-15 16:28:43 +0900

    Fix gatling virtual player load test, Add Datafaker dependency for gatling

---
+++ src/gatling/java/virtualTrader/TransactionSimulation.java
@@ -0,0 +1,71 @@
+package virtualTrader;
+
+import io.gatling.javaapi.core.CoreDsl;
+import io.gatling.javaapi.core.OpenInjectionStep;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.*;
+import net.datafaker.Faker;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static io.gatling.javaapi.core.CoreDsl.StringBody;
+import static io.gatling.javaapi.core.CoreDsl.rampUsersPerSec;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class TransactionSimulation extends Simulation {
+
+    private static HttpProtocolBuilder setupProtocol() {
+        return http.baseUrl("http://localhost:4131")
+                .acceptHeader("application/json")
+                .maxConnectionsPerHost(10)
+                .userAgentHeader("Gatling/Performance Test");
+    }
+
+    private static Iterator<Map<String, Object>> feedData() {
+
+        Faker faker = new Faker();
+
+        Random rand = new Random();
+
+        Iterator<Map<String, Object>> iterator;
+        iterator = Stream.generate(() -> {
+            Map<String, Object> stringObjectMap = new HashMap<>();
+            stringObjectMap.put("playerId", "gatling");
+            stringObjectMap.put("ticker", faker.stock().nsdqSymbol());
+            stringObjectMap.put("quantity", rand.nextLong(1000));
+            stringObjectMap.put("price", rand.nextDouble(10000));
+            return stringObjectMap;
+        })
+                .iterator();
+        return iterator;
+    }
+
+    private static ScenarioBuilder buildPostScenario() {
+        return CoreDsl.scenario("Load Test Creating Transaction")
+                .feed(feedData())
+                .exec(http("transaction").post("/order/buy")
+                        .header("Content-Type", "application/json")
+                        .body(StringBody("{\"playerId\":\"#{playerId}\",\"ticker\":\"#{ticker}\",\"quantity\":\"#{quantity}\",\"price\":\"#{price}\" }"))
+                        .check(status().is(200)));
+    }
+
+    private OpenInjectionStep.RampRate.RampRateOpenInjectionStep postEndpointInjectionProfile() {
+        int totalDesiredUserCount = 200;
+        double userRampUpPerInterval = 50;
+        double rampUpIntervalSeconds = 30;
+        int totalRampUptimeSeconds = 120;
+        int steadyStateDurationSeconds = 300;
+
+        return rampUsersPerSec(userRampUpPerInterval / (rampUpIntervalSeconds / 60)).to(totalDesiredUserCount)
+                .during(Duration.ofSeconds(totalRampUptimeSeconds + steadyStateDurationSeconds));
+    }
+
+    public TransactionSimulation() {
+        setUp(buildPostScenario().injectOpen(postEndpointInjectionProfile())
+                .protocols(setupProtocol()));
+    }
+}
